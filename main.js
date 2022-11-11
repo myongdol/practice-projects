9005427fa9ecfe09ec20fb33207af477e6cba308
myongdol <myongdol132@gmail.com>
2022-11-11 15:50:55 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+(function (window, document) {
+    'use strict';
+
+    const $toggles = document.querySelectorAll('.toggle');
+    const $toggleBtn = document.getElementById('toggle-btn');
+
+    $toggleBtn.addEventListener('click', function() {
+        toggleElments();
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 1024) {
+            offElements();
+        }
+    } );
+
+    function toggleElments () {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.toggle('on');
+        });
+    }
+    function offElements() {
+        [].forEach.call($toggles, function (toggle) {
+            toggle.classList.remove('on');
+        });    
+    }
+}) (window, document)
